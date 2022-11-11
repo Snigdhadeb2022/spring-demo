cbce37991229257ba856c0790b867b2126e3b7e9
Snigdhadeb <snigdhadeb.chatterjee@bsci.com>
2022-11-11 18:27:02 +0530

    Removed unused imports

--- src/main/java/com/application/springdemo/controller/SpringDemoController.java
+++ src/main/java/com/application/springdemo/controller/SpringDemoController.java
@@ -1,15 +1,10 @@
 package com.application.springdemo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -17,12 +12,10 @@ import java.net.UnknownHostException;
 @RequestMapping("spring-demo")
 public class SpringDemoController {
 
-    private InetAddress inetAddress;
-
     @GetMapping
     public ResponseEntity<String> hello() throws UnknownHostException {
 
-        inetAddress = InetAddress.getLocalHost();
+        InetAddress  inetAddress = InetAddress.getLocalHost();
         String hostname = inetAddress.getHostName();
         String ipAddress = String.valueOf(inetAddress)
                                     .replace(hostname,"")
